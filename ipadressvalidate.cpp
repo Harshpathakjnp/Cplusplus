c07f9ee30fece41b765ef0d67af00ef7b7f8aebe
Harsh Vardhan <92443560+Harshpathakjnp@users.noreply.github.com>
2022-04-25 11:34:14 +0530

    Add files via upload

---
+++ ipadressvalidate.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ip[3];
+    int i;
+    cout<<"Enter Your IP Adress : ";
+    for(i=0;i<=2;i++)
+    {
+    cin>>ip[i];
+    }
+    
+    
+}
